cdf0999196c309be14cb497915c8fff93bf3a4d2
Toni Powell <tonipow3ll@gmail.com>
2021-03-06 14:41:58 -0500

    setting up

--- schema.sql
+++ schema.sql
@@ -1,33 +1,43 @@
-DROP DATABASE IF EXISTS top_songsDB;
-CREATE database top_songsDB;
+DROP DATABASE IF EXISTS EmployeeTracker_DB;
+CREATE database EmployeeTracker_DB;
 
-USE top_songsDB;
+USE EmployeeTracker_DB;
 
-CREATE TABLE top5000 (
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  song VARCHAR(100) NULL,
-  year INT NULL,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
+CREATE TABLE Department (
+  id INT NOT NULL,
+  first_name VARCHAR(50) NULL,
+  last_name VARCHAR(50) NULL,
+  title VARCHAR(25) NULL,
+  department VARCHAR(30) NULL,
+  salary INT NOT NULL,
+  manager VARCHAR(25) NULL,
+  roles VARCHAR(25) NULL,
   PRIMARY KEY (position)
 );
 
-CREATE TABLE top_albums (
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  album VARCHAR(100) NULL,
-  year INT NULL,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
+CREATE TABLE Employee (
+  id INT NOT NULL,
+  first_name VARCHAR(50) NULL,
+  last_name VARCHAR(50) NULL,
+  title VARCHAR(25) NULL,
+  department VARCHAR(30) NULL,
+  salary INT NOT NULL,
+  manager VARCHAR(25) NULL,
+  roles VARCHAR(25) NULL,
   PRIMARY KEY (position)
 );
 
-SELECT * FROM top5000;
-select * from top_albums;
+CREATE TABLE Roles (
+  id INT NOT NULL,
+  first_name VARCHAR(50) NULL,
+  last_name VARCHAR(50) NULL,
+  title VARCHAR(25) NULL,
+  department VARCHAR(30) NULL,
+  salary INT NOT NULL,
+  manager VARCHAR(25) NULL,
+  roles VARCHAR(25) NULL,
+  PRIMARY KEY (position)
+);
+SELECT * FROM Department;
+select * from Employee;
+select * from Roles;
