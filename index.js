1fea06f7f9eebfb8df96af5a09cde5821aa24a9e
Toni Powell <tonipow3ll@gmail.com>
2021-03-06 15:52:57 -0500

    basic layouts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const { allowedNodeEnvironmentFlags } = require('process');
 const Employee = require("./lib/employee");
 
 const connection = mysql.createConnection({
@@ -87,9 +88,113 @@ connection.connect((err) => {
 }
 
 // const addEmployee = () => {
-//     const query = '';
+//     const query = 'INSERT INTO Employee';
+//     inquirer
 //     connection.query(query, (err, res) => {
 //         if (err) throw err;
-//      res.
+    
 //     })
 // }
+
+const addEmployee = () => {
+    inquirer
+      .prompt([
+        {  
+            type: 'input',
+            name: 'firstName',
+            message: 'Please enter employees first name',
+        },
+        {  
+            type: 'input',
+            name: 'lastName',
+            message: 'Please enter employees last name',
+        },
+        {  
+            type: 'input',
+            name: 'title',
+            message: 'Please enter employees title',
+        },
+        {  
+            type: 'input',
+            name: 'salary',
+            message: 'Please enter employees salary',
+        },
+        {  
+            type: 'input',
+            name: 'manager',
+            message: 'Please enter employees manager (hit enter to leave blank if n/a)',
+        },
+        {  
+            type: 'list',
+            name: 'department',
+            message: 'Please select a department',
+            choices: [
+                'Engineer',
+                'Manager',
+                'Finance',
+                'Exit (changes will NOT be saved)'
+            ]
+        },
+    ])
+    .then((answers) => {
+        switch (answers.department) {
+            case 'Engineer':
+                addEng();
+                break;
+            case 'Manager':
+                addMgmt();
+                break;
+            case 'Finance':
+                addAcct();
+                break;
+            case 'Exit (changes will NOT be saved)':
+                confirmExit();
+                break;
+            default:
+                initApp();
+                break;
+        }
+     })
+}
+
+// functions for adding employees below
+const addEng = (answers) => {
+    const query = 'INSERT INTO Employee'(first_name, last_name, answers.department, answers.salary, answers.manager, answers.roles);
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+      res.console.log('employee added')
+      })    
+      initApp();
+    }
+
+
+const addMgmt = () => {
+    console.log("Adding Manager!")
+    initApp();
+}
+
+const addAcct = () => {
+    console.log("Adding accountant!")
+    initApp();
+}
+
+const confirmExit = () => {
+    inquirer
+        .prompt([
+            { 
+               type: 'list',
+               name: 'exConf',
+               message: 'Are you sure you want to exit? Unsaved changes will be discarded',
+               choices: ['Yes, exit', 'No, Im not done yet']
+            }
+        ]).then((answers) => {
+            switch (answers.exConf){
+                case 'Yes, exit':
+                    connection.end(console.log("Exiting application, goodbye"));
+                break;
+                case 'No, Im not done yet':  
+                    initApp();
+                break;
+            }
+        })
+}
